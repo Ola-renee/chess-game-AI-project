b4259c198067dd73fb722fe0c9f6857448e745be
Ola-renee <85627670+Ola-renee@users.noreply.github.com>
2023-11-03 01:06:33 -0400

    Update AI.py

--- player/AI.py
+++ player/AI.py
@@ -251,7 +251,7 @@ class AI:
 
 
     def calculateb(self, gametiles):
-    piece_values = {
+        piece_values = {
             'P': -100, 'N': -320, 'B': -330, 'R': -500,
             'Q': -900, 'K': -20000,
             'p': 100, 'n': 320, 'b': 330, 'r': 500,
