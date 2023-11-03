4b273340db83aed8ff5c4634c29f97b6ded95342
Ola-renee <85627670+Ola-renee@users.noreply.github.com>
2023-11-02 23:00:14 -0400

    Update AI.py

--- player/AI.py
+++ player/AI.py
@@ -251,46 +251,35 @@ class AI:
 
 
     def calculateb(self,gametiles):
-        value=0
+    piece_values = {
+            'P': -100, 'N': -320, 'B': -330, 'R': -500,
+            'Q': -900, 'K': -20000,
+            'p': 100, 'n': 320, 'b': 330, 'r': 500,
+            'q': 900, 'k': 20000
+            }
+    value = 0
+    for y in range(8):
         for x in range(8):
-            for y in range(8):
-                    if gametiles[y][x].pieceonTile.tostring()=='P':
-                        value=value-100
-
-                    if gametiles[y][x].pieceonTile.tostring()=='N':
-                        value=value-350
-
-                    if gametiles[y][x].pieceonTile.tostring()=='B':
-                        value=value-350
-
-                    if gametiles[y][x].pieceonTile.tostring()=='R':
-                        value=value-525
-
-                    if gametiles[y][x].pieceonTile.tostring()=='Q':
-                        value=value-1000
-
-                    if gametiles[y][x].pieceonTile.tostring()=='K':
-                        value=value-10000
-
-                    if gametiles[y][x].pieceonTile.tostring()=='p':
-                        value=value+100
-
-                    if gametiles[y][x].pieceonTile.tostring()=='n':
-                        value=value+350
-
-                    if gametiles[y][x].pieceonTile.tostring()=='b':
-                        value=value+350
-
-                    if gametiles[y][x].pieceonTile.tostring()=='r':
-                        value=value+525
-
-                    if gametiles[y][x].pieceonTile.tostring()=='q':
-                        value=value+1000
-
-                    if gametiles[y][x].pieceonTile.tostring()=='k':
-                        value=value+10000
-
-        return value
+            piece_str = gametiles[y][x].pieceonTile.tostring()
+            piece_value = piece_values.get(piece_str, 0)
+            value += piece_value
+            if piece_str in ['P', 'p']:
+                if x in [2, 3, 4, 5]:
+                    value += 10 if piece_str == 'p' else -10
+                if y == 6 and piece_str == 'p':
+                    value += 50
+                if y == 1 and piece_str == 'P':
+                    value -= 50
+            if piece_str in ['N', 'n']:
+                distance_center = max(abs(3.5 - x), abs(3.5 - y))
+                value -= (8 - distance_center) * (20 if piece_str == 'n' else -20)
+            if piece_str in ['B', 'b']:
+                diagonal_dist = min(y, x, 7 - y, 7 - x)
+                value += diagonal_dist * (10 if piece_str == 'b' else -10)
+            if piece_str in ['K', 'k']:
+                if x in [0, 1, 6, 7] or y in [0, 1, 6, 7]:
+                    value -= 100 if piece_str == 'K' else 100
+    return value
 
 
     def move(self,gametiles,y,x,n,m):
